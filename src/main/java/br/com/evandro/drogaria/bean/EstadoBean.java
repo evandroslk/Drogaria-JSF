a46f49b790c2412657bb1b6f6389690cf9f33f6e
Evandro <096066330647@tre-pr.jus.br>
2022-12-15 18:46:24 -0300

    OmniFaces

--- src/main/java/br/com/evandro/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/evandro/drogaria/bean/EstadoBean.java
@@ -1,18 +1,14 @@
 package br.com.evandro.drogaria.bean;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
+
+import org.omnifaces.util.Messages;
 
 @ManagedBean
 public class EstadoBean {
 
 	public void salvar() {
-		String texto = "Programação Web com Java";
-		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_ERROR, texto, texto);
-
-		FacesContext contexto = FacesContext.getCurrentInstance();
-		contexto.addMessage(null, mensagem);
+		Messages.addGlobalError("Programação Web com Java");
 	}
 
 }
