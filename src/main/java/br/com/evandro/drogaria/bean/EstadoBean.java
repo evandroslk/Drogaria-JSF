6243c0fc905cc55225b16c2070d11de3ec7740dc
Evandro <096066330647@tre-pr.jus.br>
2022-12-15 18:41:44 -0300

    Introdução ao ManagedBean

---
+++ src/main/java/br/com/evandro/drogaria/bean/EstadoBean.java
@@ -0,0 +1,12 @@
+package br.com.evandro.drogaria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class EstadoBean {
+
+	public void salvar() {
+		System.out.println("Programação Web com Java");
+	}
+
+}
