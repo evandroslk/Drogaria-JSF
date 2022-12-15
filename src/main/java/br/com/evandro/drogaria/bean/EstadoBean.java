73256c06b1df6fe245671c3c417c0ab73c464130
Evandro <096066330647@tre-pr.jus.br>
2022-12-15 18:44:37 -0300

    FacesMessage

--- src/main/java/br/com/evandro/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/evandro/drogaria/bean/EstadoBean.java
@@ -1,12 +1,18 @@
 package br.com.evandro.drogaria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class EstadoBean {
 
 	public void salvar() {
-		System.out.println("Programação Web com Java");
+		String texto = "Programação Web com Java";
+		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_ERROR, texto, texto);
+
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, mensagem);
 	}
 
 }
