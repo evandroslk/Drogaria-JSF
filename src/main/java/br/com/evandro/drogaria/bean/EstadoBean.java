efb4199685540dd98249b090a0f80f08ff01d229
Evandro <096066330647@tre-pr.jus.br>
2022-12-16 14:29:12 -0300

    EstadoBean - Editar

--- src/main/java/br/com/evandro/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/evandro/drogaria/bean/EstadoBean.java
@@ -69,7 +69,10 @@ public class EstadoBean implements Serializable {
 			e.printStackTrace();
 		}
 		
-		
+	}
+
+	public void editar(ActionEvent evento) {
+		estado = (Estado) evento.getComponent().getAttributes().get("estadoSelecionado");
 	}
 
 	public Estado getEstado() {
