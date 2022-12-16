723dc300226af68ac2cf980f4133ec453db56775
Evandro <096066330647@tre-pr.jus.br>
2022-12-16 14:12:29 -0300

    Listagem de Estados

--- src/main/java/br/com/evandro/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/evandro/drogaria/bean/EstadoBean.java
@@ -1,7 +1,9 @@
 package br.com.evandro.drogaria.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -18,23 +20,36 @@ public class EstadoBean implements Serializable {
 
 	private Estado estado;
 
+	private List<Estado> estados;
+
 	public void novo() {
 		estado = new Estado();
+	}
+
+	@PostConstruct
+	public void listar() {
+		try {
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+		} catch (RuntimeException e) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os estados");
+			e.printStackTrace();
+		}
 	}
 
 	public void salvar() {
 		try {
 			EstadoDAO estadoDAO = new EstadoDAO();
 			estadoDAO.salvar(estado);
-			
+
 			novo();
-			
+
 			Messages.addGlobalInfo("Estado salvo com sucesso");
 		} catch (RuntimeException e) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o estado");
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public Estado getEstado() {
@@ -43,6 +58,14 @@ public class EstadoBean implements Serializable {
 
 	public void setEstado(Estado estado) {
 		this.estado = estado;
+	}
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
 	}
 
 }
